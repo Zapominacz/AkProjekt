04f44dcd0c9c4acdeaaa492005f70ca3c3327ae3
Quantunium <zapominacz@gmail.com>
2015-05-04 22:26:58 +0200

    Wstępny model pamięci

--- Symulator/src/com/zapominacz/studia/akprojekt/memory/Memory.java
+++ Symulator/src/com/zapominacz/studia/akprojekt/memory/Memory.java
@@ -9,9 +9,9 @@ import java.util.Map;
 
 public class Memory {
 
-    public final static int MEMORY_LEN = 32;
+    //public final static int MEMORY_LEN = 32;
     public final static int MEMORY_BYTES = 4;
-    public final static int CLUSTER_LEN = 128;
+    public final static int CLUSTER_LEN = 4;
 
     Map<Integer, byte[]> memoryMap;
     private static Memory instance;
@@ -30,14 +30,23 @@ public class Memory {
 
     @NotNull
     byte[] loadFromMemory(long address) {
-        int cluster = (int) (address / MEMORY_LEN);
-        int rem = (int) (address % MEMORY_LEN);
+        int cluster = (int) (address / MEMORY_BYTES);
+        int rem = (int) (address % MEMORY_BYTES);
         byte[] result;
         if(rem == 0) {
             result = memoryMap.get(cluster);
         } else {
             result = new byte[MEMORY_BYTES];
-            //TODO System.arraycopy(memoryMap.get(cluster), );
+            byte[] c1 = memoryMap.get(cluster);
+            byte[] c2 = memoryMap.get(cluster + 1);
+            if(c1 == null) {
+                memoryMap.put(cluster, new byte[]{0, 0, 0, 0});
+            }
+            if(c2 == null) {
+                memoryMap.put(cluster + 1, new byte[] {0,0,0,0});
+            }
+            System.arraycopy(c1, rem, result, 0, CLUSTER_LEN  - rem);
+            System.arraycopy(c2, 0, result, CLUSTER_LEN  - rem, rem);
         }
         return result;
     }
@@ -46,10 +55,19 @@ public class Memory {
         if(value.length != Register.WORD_LEN) {
             throw new UnsupportedOperationException("Tylko 32 bity");
         } else {
-            int cluster = (int) (address / MEMORY_LEN);
-            int rem = (int) (address % MEMORY_LEN);
+            int cluster = (int) (address / MEMORY_BYTES);
+            int rem = (int) (address % MEMORY_BYTES);
             if(rem != 0) {
-                //TODO ...
+                byte[] c1 = memoryMap.get(cluster);
+                byte[] c2 = memoryMap.get(cluster + 1);
+                if(c1 == null) {
+                    c1 =  new byte[]{0, 0, 0, 0};
+                }
+                if(c2 == null) {
+                    c2 =  new byte[]{0, 0, 0, 0};
+                }
+                System.arraycopy(value, 0, c1, rem, CLUSTER_LEN  - rem);
+                System.arraycopy(value, CLUSTER_LEN  - rem, c2, 0, rem);
             } else {
                 memoryMap.put(cluster, value);
             }
