272a1ba270e9761d6e3d286ec637f5071b8e6177
Quantunium <zapominacz@gmail.com>
2015-05-04 23:23:52 +0200

    translateAssemblyCode już trochę działa

--- Symulator/src/com/zapominacz/studia/akprojekt/Application/MnemonicCodeTranslator.java
+++ Symulator/src/com/zapominacz/studia/akprojekt/Application/MnemonicCodeTranslator.java
@@ -15,7 +15,7 @@ import java.util.Map;
 public class MnemonicCodeTranslator {
 
     private String[] asmLines;
-    private Map<String, String> opCodeMap;
+    private Map<String, String[]> opCodeMap;
 
     public MnemonicCodeTranslator() {
         try {
@@ -30,8 +30,8 @@ public class MnemonicCodeTranslator {
         Path translateTableFile = Paths.get("translateOpcodesTable.txt");
         Iterator<String> tableIterator = Files.lines(translateTableFile).iterator();
         while (tableIterator.hasNext()) {
-            String[] lineData = tableIterator.next().split(" ");
-            opCodeMap.put(lineData[0].trim(), lineData[1].trim());
+            String[] lineData = tableIterator.next().split("\\s+");
+            opCodeMap.put(lineData[0], lineData);
         }
     }
 
@@ -44,10 +44,37 @@ public class MnemonicCodeTranslator {
         }
         asmLines = asmTextPane.getText().split("\\n");
         for(String asmLine : asmLines) {
+            StringBuilder result = new StringBuilder("00000000000000000000000000001111");
+            String reg;
             String[] command = asmLine.toUpperCase().split("\\s+");
-            String opCode = opCodeMap.get(command[0]);
-            if(opCode == null) throw new UnsupportedMnemonicException("Nie ma takiego mnemonika: " + command[0]);
-            codeTextPane.insert(opCodeMap.get(command[0]), length(codeTextPane));
+            String[] code = opCodeMap.get(command[0]);
+            result.replace(0,4, code[1]);
+            try {
+                for (int i = 1; i < command.length; i++) {
+                    switch (code[i + 1]) {
+                        case "%IMM":
+                            int immediate = Integer.decode(command[i]);
+                            result.replace(20,28,Integer.toBinaryString(immediate));
+                            break;
+                        case "%IN1":
+                            reg = command[i].substring(2);
+                            result.replace(15, 20, Integer.toBinaryString(Integer.parseInt(reg, 16)));
+                            break;
+                        case "%IN2":
+                            reg = command[i].substring(2);
+                            result.replace(10,15,Integer.toBinaryString(Integer.parseInt(reg, 16)));
+                            break;
+                        case "%OUT":
+                            reg = command[i].substring(2);
+                            result.replace(5,10,Integer.toBinaryString(Integer.parseInt(reg, 16)));
+                            break;
+                    }
+                }
+            } catch (IndexOutOfBoundsException e) {
+                throw new UnsupportedOperationException("Nie ma takiej operacji: " + asmLine);
+            }
+            if(code == null) throw new UnsupportedMnemonicException("Nie ma takiego mnemonika: " + command[0]);
+            codeTextPane.insert(result.toString(), length(codeTextPane));
             insertNewLine(codeTextPane);
         }
     }
