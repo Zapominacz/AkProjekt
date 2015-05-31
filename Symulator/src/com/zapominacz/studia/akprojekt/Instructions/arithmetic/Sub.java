b5d426ca02adcd7a0f0a685d2585de3374cc976b
Quantunium <zapominacz@gmail.com>
2015-05-31 18:54:20 +0200

    Poprawiono SUB i Set/Get

--- Symulator/src/com/zapominacz/studia/akprojekt/Instructions/arithmetic/Sub.java
+++ Symulator/src/com/zapominacz/studia/akprojekt/Instructions/arithmetic/Sub.java
@@ -15,8 +15,8 @@ public class Sub extends ArithmeticInstruction {
         Bit previousCarry  = Bit.LOW;
         for (int i = 0; i < Register.WORD_LEN; i++) {
             previousCarry = carry;
-            result[i] = source1[i].xor(source2[i].xor(carry));
-            carry = (source1[i].and(source2[i].neg())).or((source1[i].neg().xor(source2[i])).and(carry));
+            result[i] = source2[i].xor(source1[i].xor(carry));
+            carry = (source2[i].and(source1[i].neg())).or((source2[i].neg().xor(source1[i])).and(carry));
         }
         if (carry != previousCarry) {
             overflow = Bit.HIGH;
